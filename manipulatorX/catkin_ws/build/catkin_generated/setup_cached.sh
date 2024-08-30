c24ae6a8b625c957840cea2c8069aec3d3561ab9
aadityanimkar <aaditya.nimkar@gmail.com>
2024-08-30 22:40:08 +0530

    ros noetic

---
+++ manipulatorX/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/aaditya20/Intro-to-robotics/manipulatorX/catkin_ws/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PWD='/home/aaditya20/Intro-to-robotics/manipulatorX/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/aaditya20/Intro-to-robotics/manipulatorX/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/aaditya20/Intro-to-robotics/manipulatorX/catkin_ws/src:/opt/ros/noetic/share'
