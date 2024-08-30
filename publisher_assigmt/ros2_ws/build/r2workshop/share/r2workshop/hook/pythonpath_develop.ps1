dffb8d431b4e923bc87622d6b6d854c93398443e
aadityanimkar <aaditya.nimkar@gmail.com>
2024-08-30 18:38:16 +0530

    build files of publisher_assi is removed for good commit

--- publisher_assigmt/ros2_ws/build/r2workshop/share/r2workshop/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/aaditya/Intro-to-robotics/publisher_assigmt/ros2_ws/build/r2workshop"
--- publisher_assigmt/ros2_ws/build/r2workshop/share/r2workshop/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/aaditya/Intro-to-robotics/publisher_assigmt/ros2_ws/build/r2workshop"
